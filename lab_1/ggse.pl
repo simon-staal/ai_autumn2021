22ff35372df6bc72c62545285b733b03ffa0d9f6
sts219 <sts219@ic.ac.uk>
2021-11-02 16:17:08 +0000

    T2.3 untested, starting Lab1

---
+++ lab_1/ggse.pl
@@ -0,0 +1,35 @@
+% A Graph is a list of paths
+% A path is a list of nodes
+% A node is your state-space representation
+search(Graph, [Node|Path]):-
+	choose([Node|Path], Graph,_),
+	state_of(Node, State),
+	goal_state(State)
+    . % This cut is for breadth-first search only, find 1 optimal solution
+
+search(Graph, SolnPath) :-
+	choose(Path, Graph, OtherPaths), % Defined in graph search algorithm
+	one_step_extensions( Path, NewPaths),
+	add_to_paths(NewPaths, OtherPaths, GraphPlus), % Also defined in graph search algorithm
+	search(GraphPlus, SolnPath).
+
+one_step_extensions([Node|Path], NewPaths):-
+	state_of(Node, State),
+	findall([NewNode,Node|Path],
+		(state_change(Rule, State, NewState), % Defined in state-space representation
+		 new_state_on_path( NewState, Path ),
+		 make_node(Rule, NewState, NewNode)), % Also defined in state-space representation
+	NewPaths).
+
+new_state_on_path( _, [] ).
+new_state_on_path( State, [(State,_,_,_)|_] ) :-
+	!, fail.
+new_state_on_path( State, [_|Path] ) :-
+	new_state_on_path( State, Path ).
+
+write_graph( [] ) :-
+	write( '=====' ), nl.
+write_graph( [H|T] ) :-
+	write( H ), nl,
+	write_graph( T ).
+
--- tutorial_2/ex_3.pl
+++ tutorial_2/ex_3.pl
@@ -2,13 +2,52 @@
 
 You are also given a weighing maachine with two pans. The machine tells you which of the 2 pans is heavier, but you can only use it twice. Find how to weigh the balls so that you can identify which one is the heavier one.
 
-State space representation:
-[x, x, x, x, x, x, x]
-7 entries in array for 7 balls
-x represents unknown
-l represents light
-h represents heavy
-
 scale can lean left, right, or middle
+
+State representation
+(List1, List2, List3, N)
+List1 is Balls not in a Pan
+List2 is LeftPan
+List3 is RightPan
+N is number of weighs
+
+Start state
+([1, 2, 3, 4, 5, 6, 7], [], [], 0)
+
+Goal State
+(L, [], [], N), length(L, 1), N < 3
+
+Actions:
+-Distribute
+-Weight
+
 */
+
+state_change(distribute, (OldP, [], [], N), (NewP, LP, RP, N)) :-
+    append(LP, Temp, OldP),
+    append(RP, NewP, Temp),
+    length(LP, X),
+    length(RP, X).
+
+state_change(weigh, (OldP, LP, RP, N), (NewP, [], [], N1)) :-
+    weigh(LP, RP, Result),
+    in_pan_or_out(Result, OldP, NewP),
+    N1 is N + 1.
+
+in_pan_or_out([], OldP, OldP) :- !.
+in_pan_or_out(L, _, L).
+
+weigh(LP, _, LP):- 
+    htto(H),
+    member(H, LP),
+    !.
+
+weigh(_, RP, RP) :-
+    htto(H),
+    member(H, RP),
+    !.
+
+weigh(_, _, []).
+
+htto(4). % Defines which ball is the heaviest
 
